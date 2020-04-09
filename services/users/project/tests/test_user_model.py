618933dfc5e148ad805af95cbabfd04caaec18fd
Todd Leonhardt <todd.leonhardt@gmail.com>
2020-04-09 00:57:21 -0400

    Improve code coverage

--- services/users/project/tests/test_user_model.py
+++ services/users/project/tests/test_user_model.py
@@ -16,6 +16,6 @@ def test_encode_token(test_app, test_database, add_user):
 
 def test_decode_token(test_app, test_database, add_user):
     user = add_user("justatest", "test@test.com", "test")
-    token = user.encode_token(user.id, "access")
+    token = user.encode_token(user.id, "refresh")
     assert isinstance(token, bytes)
     assert User.decode_token(token) == user.id
