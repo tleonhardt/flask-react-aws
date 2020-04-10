01a02f2f329b9ad5dbf7b31778bcfcafd940d386
Todd Leonhardt <tleonha@amazon.com>
2020-04-10 13:59:19 -0400

    Added form code for RegisterForm and LoginForm components

--- services/client/src/components/__tests__/LoginForm.test.jsx
+++ services/client/src/components/__tests__/LoginForm.test.jsx
@@ -1,16 +1,35 @@
-import React from "react";
-import { render, cleanup } from "@testing-library/react";
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
 
-import LoginForm from "../LoginForm";
+import LoginForm from '../LoginForm';
 
 afterEach(cleanup);
 
-it("renders properly", () => {
-  const { getByText } = render(<LoginForm />);
-  expect(getByText("Log In")).toHaveClass("title");
+const props = {
+  handleLoginFormSubmit: () => { return true },
+}
+
+it('renders properly', () => {
+  const { getByText } = render(<LoginForm {...props} />);
+  expect(getByText('Log In')).toHaveClass('title');
+});
+
+it('renders with default props', () => {
+  const { getByLabelText, getByText } = render(<LoginForm {...props} />);
+
+  const emailInput = getByLabelText('Email');
+  expect(emailInput).toHaveAttribute('type', 'email');
+  expect(emailInput).not.toHaveValue();
+
+  const passwordInput = getByLabelText('Password');
+  expect(passwordInput).toHaveAttribute('type', 'password');
+  expect(passwordInput).not.toHaveValue();
+
+  const buttonInput = getByText('Submit');
+  expect(buttonInput).toHaveValue('Submit');
 });
 
 it("renders", () => {
-  const { asFragment } = render(<LoginForm />);
+  const { asFragment } = render(<LoginForm {...props} />);
   expect(asFragment()).toMatchSnapshot();
 });
--- services/client/src/components/__tests__/RegisterForm.test.jsx
+++ services/client/src/components/__tests__/RegisterForm.test.jsx
@@ -1,16 +1,39 @@
-import React from "react";
-import { render, cleanup } from "@testing-library/react";
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
 
-import RegisterForm from "../RegisterForm";
+import RegisterForm from '../RegisterForm';
 
 afterEach(cleanup);
 
-it("renders properly", () => {
-  const { getByText } = render(<RegisterForm />);
-  expect(getByText("Register")).toHaveClass("title");
+const props = {
+  handleRegisterFormSubmit: () => { return true },
+}
+
+it('renders properly', () => {
+  const { getByText } = render(<RegisterForm {...props} />);
+  expect(getByText('Register')).toHaveClass('title');
+});
+
+it('renders with default props', () => {
+  const { getByLabelText, getByText } = render(<RegisterForm {...props} />);
+
+  const usernameInput = getByLabelText('Username');
+  expect(usernameInput).toHaveAttribute('type', 'text');
+  expect(usernameInput).not.toHaveValue();
+
+  const emailInput = getByLabelText('Email');
+  expect(emailInput).toHaveAttribute('type', 'email');
+  expect(emailInput).not.toHaveValue();
+
+  const passwordInput = getByLabelText('Password');
+  expect(passwordInput).toHaveAttribute('type', 'password');
+  expect(passwordInput).not.toHaveValue();
+
+  const buttonInput = getByText('Submit');
+  expect(buttonInput).toHaveValue('Submit');
 });
 
 it("renders", () => {
-  const { asFragment } = render(<RegisterForm />);
+  const { asFragment } = render(<RegisterForm {...props} />);
   expect(asFragment()).toMatchSnapshot();
 });
